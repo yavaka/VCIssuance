f4f9c912c1bd33ce6cabe81cb17afdc1b0e000da
yavaka <yavakasolutions@gmail.com>
2025-11-15 16:30:01 +0000

    Init commit

---
+++ VCIssuance.Api/Configurations/ApiConfigurations.cs
@@ -0,0 +1,12 @@
+﻿namespace VCIssuance.Api.Configurations;
+
+public static class ApiConfigurations
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddOpenApi();
+
+        return services;
+    }
+}
---
+++ VCIssuance.Api/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
